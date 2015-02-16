5ee949869ca809fb44bc2a6319fa9385335180a1
bkn28 <bnguyen.mail@gmail.com>
2015-02-16 18:33:17 -0500

    Food.swift

---
+++ Food.swift
@@ -0,0 +1,15 @@
+//
+//  Food.swift
+//  Trenu
+//
+//  Created by Brandon Nguyen on 2/16/15.
+//  Copyright (c) 2015 Trenu. All rights reserved.
+//
+
+struct Food {
+    let category : String
+    let name : String
+}
+
+
+import Foundation
